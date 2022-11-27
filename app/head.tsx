27e8edadc05053a84fedbd0e074f86e6608f1bd1
Luke Mendelman-Haenn <glukemh@gmail.com>
2022-11-27 13:57:29 -0700

    home page

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+const Head = () => {
+	return (
+		<head>
+			<title>Number Ipsum</title>
+			<meta charSet="utf-8" />
+			<meta name="description" content="A blog about math and programing" />
+			<meta name="keywords" content="Math, Code, Programming" />
+			<meta name="author" content="Luke Mendelman-Haenn" />
+			<link rel="icon" href="/favicon.ico" />
+		</head>
+	);
+};
+
+export default Head;

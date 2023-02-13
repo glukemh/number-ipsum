8a95a1c66793f58f5e2146cee1cec069500d0810
GLukeM-H <glukemh@gmail.com>
2023-02-12 18:21:34 -0600

    scroll snap grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import CarouselGrid from "components/server/carousel-grid";
+
 const Page = () => {
-	return <h1>Number Ipsum</h1>;
+	return (
+		<CarouselGrid>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+		</CarouselGrid>
+	);
 };
 
 export default Page;
--- components/server/carousel-grid/CarouselGrid.tsx
+++ components/server/carousel-grid/CarouselGrid.tsx
@@ -1,11 +1,25 @@
+import styles from "./styles.module.css";
+
 type CarouselGridProps = {
-	children: React.ReactNode[];
+	children: React.ReactNode | React.ReactNode[];
 };
 
 const CarouselGrid = ({ children }: CarouselGridProps) => {
+	const childrenArray = Array.isArray(children) ? children : [children];
+	const sqrt = Math.sqrt(childrenArray.length);
+	const sqrtFloor = Math.floor(sqrt);
+	const sqrtCeil = Math.ceil(sqrt);
+	const [length, width] =
+		sqrtFloor * sqrtCeil >= childrenArray.length
+			? [sqrtFloor, sqrtCeil]
+			: [sqrtCeil, sqrtCeil];
+	const style = {
+		"--repeat-row": length,
+		"--repeat-col": width,
+	} as React.CSSProperties;
 	return (
-		<ul>
-			{children.map((child, index) => (
+		<ul className={styles.grid} style={style}>
+			{childrenArray.map((child, index) => (
 				<li key={index}>{child}</li>
 			))}
 		</ul>

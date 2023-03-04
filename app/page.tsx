afd74767209eda06766c00093f0578e7d72bc323
Luke Mendelman-Haenn <glukemh@gmail.com>
2023-03-03 23:34:05 -0600

    MathML

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 import CarouselGrid from "components/carousel-grid";
-import HelloWorld, { meta } from "posts/hello.mdx";
 
 const Page = () => {
 	return (
-		<>
-			<h1>{meta.title}</h1>
-			<HelloWorld />
-		</>
-		// <CarouselGrid>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// 	<h1>Number Ipsum</h1>
-		// </CarouselGrid>
+		<CarouselGrid>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+			<h1>Number Ipsum</h1>
+		</CarouselGrid>
 	);
 };
 

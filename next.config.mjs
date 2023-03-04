082f42eca161f6ded99098b6e79b4012a18c7a04
Luke Mendelman-Haenn <glukemh@gmail.com>
2023-03-04 01:23:48 -0600

    remove mdx for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,12 @@
-import mdx from "@next/mdx";
-import remarkGfm from "remark-gfm";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+	pageExtensions: ["ts", "tsx", "js", "jsx"],
 	experimental: {
 		appDir: true,
 		runtime: "experimental-edge",
 	},
 };
 
-const withMDX = mdx({
-	extension: /\.mdx?$/,
-	options: {
-		remarkPlugins: [remarkGfm],
-	},
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;

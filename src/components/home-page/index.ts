a193f8a3c1c621721a4c16e3c5c6a4dad0bb3032
GLukeM-H <glukemh@gmail.com>
2023-05-21 20:28:31 -0500

    scroll timeline

--- src/components/home-page/index.ts
+++ src/components/home-page/index.ts
@@ -1,20 +1,35 @@
 import "/assets/adopt-sheets";
 import templateStr from "./template.fragment.html?raw";
+import styles from "./styles.css?raw";
 
 const template = document.createElement("template");
 template.innerHTML = templateStr;
 
 class HomePage extends HTMLElement {
 	static content = template.content;
+	static styles = styles;
 	content = HomePage.content.cloneNode(true) as DocumentFragment;
 	shadow = this.attachShadow({ mode: "open" });
+	styleSheet = new CSSStyleSheet();
+	ul = this.content.querySelector("ul") as HTMLUListElement;
+	scrollEl = this.content.getElementById("scroll") as HTMLDivElement;
 	constructor() {
 		super();
-		this.shadow.adoptedStyleSheets = [...document.adoptedStyleSheets];
+		this.styleSheet.replaceSync(HomePage.styles);
+		this.shadow.adoptedStyleSheets = [
+			...document.adoptedStyleSheets,
+			this.styleSheet,
+		];
 		this.shadow.append(this.content);
+	}
+	connectedCallback() {
+		this.scrollEl.scrollLeft = this.scrollEl.getBoundingClientRect().width;
+		this.scrollEl.scrollTop = this.scrollEl.getBoundingClientRect().height;
 	}
 }
 
-customElements.define("home-page", HomePage);
+if (!customElements.get("home-page")) {
+	customElements.define("home-page", HomePage);
+}
 
 export default HomePage;

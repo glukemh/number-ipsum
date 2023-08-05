2e4cb44f6aa8a9d02e900e1d54720704d8f6161c
GLukeM-H <glukemh@gmail.com>
2023-08-05 11:11:45 -0500

    remove shadow element from mixins

--- src/assets/mixins.ts
+++ src/assets/mixins.ts
@@ -5,28 +5,6 @@ type RangeTemplateMixin = {
 	};
 };
 
-export default class ShadowDOMElement extends HTMLElement {
-	shadow: ShadowRoot;
-	constructor() {
-		super();
-
-		if (this.shadowRoot) {
-			this.shadow = this.shadowRoot;
-		} else {
-			const template = this.querySelector(
-				"template[shadowrootmode]"
-			) as HTMLTemplateElement;
-			const attributeMode = template.getAttribute("shadowrootmode");
-			const mode: ShadowRootMode =
-				attributeMode === "open" || attributeMode === "closed"
-					? attributeMode
-					: "open";
-			this.shadow = this.attachShadow({ mode });
-			this.shadow.appendChild(template.content);
-		}
-	}
-}
-
 export const rangeTemplateMixin = <T extends new (...args: any[]) => any>(
 	Base: T
 ): RangeTemplateMixin & T => {
